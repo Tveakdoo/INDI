fd913c3ebc4161f934512fe9bb81cd4b65ed81b4
Tveakdoo <ilya@uni-a.ru>
2022-05-18 15:23:21 +0300

    INDI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+  import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components"
+  const Title = styled.main`
+    background-image: url("../public/img/logo.png");
+  `
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <main>
+          <Title>
+
+          </Title>
+      </main>
     </div>
   );
 }
